d536b72a5c34dcd549c0aefca736d91762575554
Irfan <irfanali987654@gmail.com>
2021-07-04 23:29:29 +0530

    style addded

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Post from './Post/Post'
+import Post from './Post/Post';
+import useStyles from './style';
 const Posts= ()=>{
+    const classes = useStyles();
     return (
         <>
         <h1>Posts</h1>
